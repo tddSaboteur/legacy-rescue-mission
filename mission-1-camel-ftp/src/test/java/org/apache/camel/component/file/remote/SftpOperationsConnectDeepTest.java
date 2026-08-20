d9fb695decb73af51c7f348e1835d261353f5177
antizep <307711993+tddSaboteur@users.noreply.github.com>
2026-08-20 23:34:42 +0300

    dock: Проверяем повторные попытки соединений.

---
+++ mission-1-camel-ftp/src/test/java/org/apache/camel/component/file/remote/SftpOperationsConnectDeepTest.java
@@ -0,0 +1,46 @@
+package org.apache.camel.component.file.remote;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.component.file.GenericFileEndpoint;
+import org.apache.camel.component.file.GenericFileOperationFailedException;
+import org.apache.camel.component.file.remote.exception.SftpClientException;
+import org.apache.camel.component.file.remote.gateway.SftpClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SftpOperationsConnectDeepTest {
+
+    SftpOperations sftpOperations;
+    @Mock
+    private SftpEndpoint endpoint;
+    @Mock
+    SftpConfiguration configuration;
+    @Mock
+    SftpClient sftpClient;
+
+    @BeforeEach
+    void setUp() {
+        sftpOperations = new SftpOperations(sftpClient);
+        sftpOperations.setEndpoint(endpoint);
+    }
+
+
+    @Test
+    public void connect_widthRetryConfiguration_shouldTryToConnect(){
+        when(endpoint.getMaximumReconnectAttempts()).thenReturn(10);
+        doThrow(SftpClientException.class)
+                .when(sftpClient).init(any());
+
+        assertThrows(GenericFileOperationFailedException.class,() -> sftpOperations.connect(configuration,null));
+
+        verify(sftpClient,times(10)).init(any());
+    }
+
+}
