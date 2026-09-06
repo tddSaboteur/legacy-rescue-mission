35bcbc9ef55bef2a8909e403e28d44ab2afd3590
tddSaboteur <tdd.saboteur@yandex.ru>
2026-09-06 22:00:19 +0300

    feature: Реализовал правильное заполнение сущности.

--- mission-1-camel-ftp/src/main/java/org/apache/camel/component/file/remote/gateway/SftpFileMetadata.java
+++ mission-1-camel-ftp/src/main/java/org/apache/camel/component/file/remote/gateway/SftpFileMetadata.java
@@ -1,11 +1,17 @@
 package org.apache.camel.component.file.remote.gateway;
 
 import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.SftpATTRS;
 
-public record SftpFileMetadata(String filename,String longname,SftpATTRS attrs) {
+public record SftpFileMetadata(String filename, String longname, long length, long MTime, boolean dir) {
 
-    static void fromLsEntry(ChannelSftp.LsEntry entry) {
+    static SftpFileMetadata fromLsEntry(ChannelSftp.LsEntry entry) {
 
+        return new SftpFileMetadata(
+                entry.getFilename(),
+                entry.getLongname(),
+                entry.getAttrs().getSize(),
+                entry.getAttrs().getMTime(),
+                entry.getAttrs().isDir()
+        );
     }
 }
