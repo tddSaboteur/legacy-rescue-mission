c6d0e3c4bbcf21e5f0f3a0e18cf9220a85356a7a
tddSaboteur <tdd.saboteur@yandex.ru>
2026-09-06 21:57:35 +0300

    test: Тестируем правильность заполнения сущности.

--- mission-1-camel-ftp/src/test/java/org/apache/camel/component/file/remote/gateway/SftpFileMetadataTest.java
+++ mission-1-camel-ftp/src/test/java/org/apache/camel/component/file/remote/gateway/SftpFileMetadataTest.java
@@ -1,19 +1,53 @@
 package org.apache.camel.component.file.remote.gateway;
 
 import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.SftpATTRS;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SftpFileMetadataTest {
 
     @Mock
     ChannelSftp.LsEntry entry;
+    @Mock
+    SftpATTRS attrs;
 
     @Test
-    void testMapping(){
+    void testMapping_empty_entry(){
         SftpFileMetadata.fromLsEntry(entry);
+    }
+
+    @Test
+    void testMapping_configured_entry(){
+        Boolean isDir = Boolean.FALSE;
+        int mTime = 1;
+        long size = 11L;
+        String filename = "FILENAME";
+        String longname = "LONG_NAME";
+
+        when(entry.getAttrs()).thenReturn(attrs);
+        when(entry.getFilename()).thenReturn(filename);
+        when(entry.getLongname()).thenReturn(longname);
+        when(attrs.isDir()).thenReturn(isDir);
+        when(attrs.getMTime()).thenReturn(mTime);
+        when(attrs.getSize()).thenReturn(size);
+
+        SftpFileMetadata metadata = SftpFileMetadata.fromLsEntry(entry);
+
+        assert metadata != null;
+        assertEquals(metadata.dir(),isDir);
+        assertEquals(metadata.MTime(), mTime);
+        assertEquals(metadata.length(),size);
+        assertEquals(metadata.filename(),filename);
+        assertEquals(metadata.longname(),longname);
+
+
     }
 }
